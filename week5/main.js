b2ed4080ec7214d5cdf70b182b1fd4c53a87148c
smcatingub <smcatingub@student.apc.edu.ph>
2024-01-23 22:52:15 +0800

    removed the lesson comments for a cleaner code

--- week5/main.js
+++ week5/main.js
@@ -1,27 +1,24 @@
-class ChatManager { //Lesson 20. Classes
-    constructor() { //Lesson 20. Classes
-        this.currentDivId = 'chat1'; //Lesson 20. Classes
-        this.clickId = 'click-to-continue'; //Lesson 20. Classes
-    }
-    showNext() { //Lesson 18. Functions
-        const currentDiv = document.getElementById(currentDivId); //Lesson 17. Objects & Lesson 22. HTML DOM
-        const currentDivNum = parseInt(currentDivId.slice(-1)); //Lesson 17. Objects
-        const nextDivNum = currentDivNum + 1; //Lesson 17. Objects
-        const nextDivId = `chat${nextDivNum}`; //Lesson 17. Objects
+let currentDivId = 'chat1'; 
+let clickId = 'click-to-continue'; 
 
-        if (currentDiv && nextDivNum <= 4) {
-            const nextDiv = document.getElementById(nextDivId); //Lesson 17. Objects & Lesson 22. HTML DOM
-            if (nextDiv) {
-                setTimeout(() => { //Lesson 21. Async
-                    nextDiv.classList.remove('hidden'); //Lesson 21. Async
-                    currentDivId = nextDivId; //Lesson 21. Async
-                }, 250) //Lesson 21. Async
-            }
-        } else {
-            const clickElement = document.getElementById(clickId); //Lesson 17. Objects & Lesson 22. HTML DOM
-            if (clickElement) {
-            clickElement.classList.add('hidden');
-            }
+function showNext() { 
+    const currentDiv = document.getElementById(currentDivId); 
+    const currentDivNum = parseInt(currentDivId.slice(-1)); 
+    const nextDivNum = currentDivNum + 1; 
+    const nextDivId = `chat${nextDivNum}`; 
+
+    if (currentDiv && nextDivNum <= 4) {
+        const nextDiv = document.getElementById(nextDivId); 
+        if (nextDiv) {
+            setTimeout(() => { 
+                nextDiv.classList.remove('hidden'); 
+                currentDivId = nextDivId; 
+            }, 250) 
+        }
+    } else {
+        const clickElement = document.getElementById(clickId); 
+        if (clickElement) {
+        clickElement.classList.add('hidden');
         }
     }
 }
