5af0c75305c6f81f35fc73a9f7076a029834a2a7
smcatingub <smcatingub@student.apc.edu.ph>
2024-01-31 21:33:03 +0800

    did initial layout for about page

--- week8/contact_form.php
+++ week8/contact_form.php
@@ -137,5 +137,30 @@
             echo $gender;
             ?>
 
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "myDB";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+            VALUES ('$name', '$website', '$email')";
+
+            if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+            ?>
+
         </div>
     </div>
