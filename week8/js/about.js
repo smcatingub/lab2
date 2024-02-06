16510ddd70ab18c4be810e76c6c990eaf4acb77d
smcatingub <smcatingub@student.apc.edu.ph>
2024-02-06 18:29:57 +0800

    joke eto pala final ng educ

--- week8/js/about.js
+++ week8/js/about.js
@@ -5,11 +5,20 @@ var btn_future = document.getElementById("future");
 
 var div_select = document.getElementById("select");
 var div_aboutbasic = document.getElementById("about-basic");
+var div_abouteduc = document.getElementById("about-educ");
 // var div_ = document.getElementById("");
 // var div_ = document.getElementById("");
 
 btn_basic.addEventListener("click", ()=>{
     div_select.style.visibility = "hidden";
+    div_abouteduc.style.visibility = "hidden";
     div_aboutbasic.style.visibility = "visible";
     div_aboutbasic.style.opacity = "1";
+});
+
+btn_educ.addEventListener("click", ()=>{
+    div_select.style.visibility = "hidden";
+    div_aboutbasic.style.visibility = "hidden";
+    div_abouteduc.style.visibility = "visible"
+    div_abouteduc.style.opacity = "1";
 });
