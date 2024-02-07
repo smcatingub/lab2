3f6e9ed83233b4d309b7f3f5f2cbbc2b222b191f
smcatingub <smcatingub@student.apc.edu.ph>
2024-02-07 18:51:49 +0800

    added whyIT page

--- week8/js/about.js
+++ week8/js/about.js
@@ -6,19 +6,53 @@ var btn_future = document.getElementById("future");
 var div_select = document.getElementById("select");
 var div_aboutbasic = document.getElementById("about-basic");
 var div_abouteduc = document.getElementById("about-educ");
-// var div_ = document.getElementById("");
-// var div_ = document.getElementById("");
+var div_aboutwhyit = document.getElementById("about-whyit");
+var div_aboutfuture = document.getElementById("about-future");
 
 btn_basic.addEventListener("click", ()=>{
     div_select.style.visibility = "hidden";
-    div_abouteduc.style.visibility = "hidden";
+
     div_aboutbasic.style.visibility = "visible";
     div_aboutbasic.style.opacity = "1";
+
+    div_abouteduc.style.visibility = "hidden";
+    div_abouteduc.style.opacity = "0";
+
+    div_aboutwhyit.style.visibility = "hidden"
+    div_aboutwhyit.style.opacity = "0";
+
+    div_aboutfuture.style.visibility = "hidden";
+    div_aboutfuture.style.opacity = "0";
 });
 
 btn_educ.addEventListener("click", ()=>{
     div_select.style.visibility = "hidden";
+
     div_aboutbasic.style.visibility = "hidden";
+    div_aboutbasic.style.opacity = "0";
+
     div_abouteduc.style.visibility = "visible"
     div_abouteduc.style.opacity = "1";
+
+    div_aboutwhyit.style.visibility = "hidden"
+    div_aboutwhyit.style.opacity = "0";
+
+    div_aboutfuture.style.visibility = "hidden";
+    div_aboutfuture.style.opacity = "0";
+});
+
+btn_whyIT.addEventListener("click", ()=>{
+    div_select.style.visibility = "hidden";
+
+    div_aboutbasic.style.visibility = "hidden";
+    div_aboutbasic.style.opacity = "0";
+
+    div_abouteduc.style.visibility = "hidden";
+    div_abouteduc.style.opacity = "0";
+
+    div_aboutwhyit.style.visibility = "visible"
+    div_aboutwhyit.style.opacity = "1";
+
+    div_aboutfuture.style.visibility = "hidden";
+    div_aboutfuture.style.opacity = "0";
 });
