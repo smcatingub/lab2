b2930b4593b2839f834f4ae86efab7bb7ce07d50
smcatingub <smcatingub@student.apc.edu.ph>
2024-02-20 17:25:10 +0800

    fixed contact page css

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,16 +1,16 @@
 <?php
 
 //for xampp
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "myDB";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
 
 //for socitcloud
-$servername = "localhost";
-$username = "webprogmi222_sf221";
-$password = "xE*Y2nleNVvZm[!!";
-$dbname = "webprogmi222_sf221";
+//$servername = "localhost";
+//$username = "webprogmi222_sf221";
+//$password = "xE*Y2nleNVvZm[!!";
+//$dbname = "webprogmi222_sf221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,13 +19,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM smcatingub_myguests";
+$sql = "SELECT id, name, email, comment, gender FROM myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. " - Website: " . $row["website"]. " - Comment: " . $row["comment"]. " - Gender: " . $row["gender"]. "<br>";
+    echo "Name: " . $row["name"]. "<br>Email: " . $row["email"]. "<br>Comment: " . $row["comment"]. "<br>Gender: " . $row["gender"]. "<br>";
   }
 } else {
   echo "0 results";
